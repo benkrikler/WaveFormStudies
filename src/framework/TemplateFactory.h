d662abdb5a4b97284ce345840a738d1041abe9ed
benkrikler <mr.krikler@googlemail.com>
2014-08-20 14:43:00 +0100

    Code indenting tidied up for TemplateFactory

--- src/framework/TemplateFactory.h
+++ src/framework/TemplateFactory.h
@@ -12,78 +12,78 @@
 template <typename BaseModule, typename OptionsType>
 class TemplateFactory{
     protected:
-	/// protected constructor only callable by derived class 
-	/// as this shoule be a singleton class
-	TemplateFactory(){};
-	virtual ~TemplateFactory(){
-	    fModuleMakers.clear();
-	    fModuleOptions.clear();
-	}
+        /// protected constructor only callable by derived class 
+        /// as this shoule be a singleton class
+        TemplateFactory(){};
+        virtual ~TemplateFactory(){
+            fModuleMakers.clear();
+            fModuleOptions.clear();
+        }
 
     public:
-	// typdef a function pointer for creating a module
-	typedef BaseModule*(*maker)(OptionsType*);
-	/// Add a maker function to the list of makers
-	void registerModule(const std::string&, maker);
-	/// Create a module with options
-	virtual BaseModule* createModule(const std::string&, OptionsType*);
-    /// Check if a module with this name has been registered
-	bool canCreate(const std::string& name)const{return (bool) fModuleMakers.count(name);};
-	/// Create a module and look up its options from the internal list
-	BaseModule* createModule(const std::string& name);
-	void addOptions(const std::string& name, OptionsType *opts);
-	void addArguments(const std::string& all_args);
-	void addArgument(const std::string& module,const std::string& argument);
-	void addArgument(const std::string& argument);
-	std::string GetArgumentName(const std::string& module,const int& argument);
+        // typdef a function pointer for creating a module
+        typedef BaseModule*(*maker)(OptionsType*);
+        /// Add a maker function to the list of makers
+        void registerModule(const std::string&, maker);
+        /// Create a module with options
+        virtual BaseModule* createModule(const std::string&, OptionsType*);
+        /// Check if a module with this name has been registered
+        bool canCreate(const std::string& name)const{return (bool) fModuleMakers.count(name);};
+        /// Create a module and look up its options from the internal list
+        BaseModule* createModule(const std::string& name);
+        void addOptions(const std::string& name, OptionsType *opts);
+        void addArguments(const std::string& all_args);
+        void addArgument(const std::string& module,const std::string& argument);
+        void addArgument(const std::string& argument);
+        std::string GetArgumentName(const std::string& module,const int& argument);
 
-	// Set the factory to output debugging commands
-	void SetDebug(bool debug=true){fDebug=debug;};
+        // Set the factory to output debugging commands
+        void SetDebug(bool debug=true){fDebug=debug;};
 
-  void PrintPossibleModules()const;
+        void PrintPossibleModules()const;
 
     private:
-	// the list of makers for all modules
-	typedef std::map<std::string, maker> MakersList; 
-	MakersList fModuleMakers;
+        // the list of makers for all modules
+        typedef std::map<std::string, maker> MakersList; 
+        MakersList fModuleMakers;
 
-	// the list of given options for all modules
-	typedef std::map<std::string, OptionsType*> OptionsList; 
-	OptionsList fModuleOptions;
+        // the list of given options for all modules
+        typedef std::map<std::string, OptionsType*> OptionsList; 
+        OptionsList fModuleOptions;
 
-	// the list of expected arguments for all modules
-	typedef std::vector<std::string> ArgumentsVector;
-	typedef std::map<std::string, ArgumentsVector> ArgsList; 
-	ArgsList fModuleArguments;
+        // the list of expected arguments for all modules
+        typedef std::vector<std::string> ArgumentsVector;
+        typedef std::map<std::string, ArgumentsVector> ArgsList; 
+        ArgsList fModuleArguments;
 
-	// the most recent module to have been registered
-	std::string fMostRecentRegister;
+        // the most recent module to have been registered
+        std::string fMostRecentRegister;
 
-	// flag to control verbosity
-	bool fDebug;
+        // flag to control verbosity
+        bool fDebug;
 };
 
 #include "TemplateFactory.tpl"
 
 
 template <typename ConcreteClass,
-	 typename BaseClass,
-	 typename Options>
- BaseClass* RegistryProxyMaker(Options* opts){return new ConcreteClass(opts);};
+         typename BaseClass,
+         typename Options>
+         BaseClass* RegistryProxyMaker(Options* opts){return new ConcreteClass(opts);};
 
 template <typename ConcreteClass,
-	 typename BaseClass,
-	 typename Options,
-	 typename Factory> 
-class RegistryProxy{
-  public: 
-    RegistryProxy(const char* Name, const char* ArgumentNames) 
-    { 
-      Factory *f = Factory::Instance(); 
-      f->registerModule( Name , &RegistryProxyMaker<ConcreteClass,BaseClass,Options>); 
-      /* Add all the arguments to the manager */
-      f->addArguments( ArgumentNames);
-    }
-};
+         typename BaseClass,
+         typename Options,
+         typename Factory> 
+         class RegistryProxy{
+             public: 
+                 RegistryProxy(const char* Name, const char* ArgumentNames) 
+                 { 
+                     Factory *f = Factory::Instance(); 
+                     f->registerModule( Name , &RegistryProxyMaker<ConcreteClass,BaseClass,Options>); 
+                     /* Add all the arguments to the manager */
+                     f->addArguments( ArgumentNames);
+                 }
+         };
 
 #endif //TEMPLATEFACTORY_H_
