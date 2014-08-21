6b094ddc14be2d538eea3820a7e11a029a477940
benkrikler <mr.krikler@googlemail.com>
2014-08-21 06:14:00 +0100

    use the updated WaveformGenerator in WaveformSource

--- src/modules/WaveformSource.cpp
+++ src/modules/WaveformSource.cpp
@@ -1,40 +1,42 @@
 #include "WaveformSource.h"
 #include "ModulesFactory.h"
+#include "GeneratorsFactory.h"
+#include "GeneratorsOptions.h"
 #include "wfsDefinitions.h"
 #include "wfsLog.h"
 #include "wfsRunManager.h"
+#include "wfsWaveform.h"
 
 #include "WaveformGenerator.h"
-#include "LoadOldWaveforms.h"
 
 using std::cout;
 using std::endl;
 
 WaveformSource::WaveformSource(modules::options* opts):
    BaseModule("WaveformSource",opts),fOptions(opts),fGenerator(NULL){
-       fMode=opts->GetString("mode","");
+       fMode=opts->GetString("mode");
 }
 
 WaveformSource::~WaveformSource(){
 }
 
 int WaveformSource::BeforeFirstEntry(){
-    if(fMode=="load_old"){
-        WFSOut(2)<<"Obtaining waveforms from old files"<<endl;
-        fGenerator=new LoadOldWaveforms(fOptions);
-    }else {
+    // Set up the generator
+    fGenerator=generators::factory::Instance()->createModule(fMode,new generators::options(*fOptions));
+    if(!fGenerator) {
         WFSErr(1)<<"Unknown mode: '"<<fMode<<"'"<<endl;
         return 1;
     }
-
     WFS::RunManager::Instance()->NumberEventsToProcess(fGenerator->GetNumWFS());
-
   return 0;
 }
 
 int WaveformSource::ProcessEntry(bool& go_on){
-  return fGenerator->GetWaveform();
-  return 0;
+    try{
+        const WFS::WaveForm& waveform=fGenerator->GetWaveform();
+        WFS::RunManager::SetGlobalWaveform("input",waveform);
+    } catch( WFS::Errors::stop_generating& e){}
+    return 0;
 }
 
 int WaveformSource::AfterLastEntry(){
